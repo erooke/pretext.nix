48272efe8d877a6f2c14135ec049e1b59a87b8f0
Ethan Rooke <ethan@roo.ke>
2025-01-30 21:56:54 -0600

    pdfcropmargins: init at 1.0.9

---
+++ pdfcropmargins.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  pythonOlder,
+  fetchPypi,
+  # Build
+  setuptools,
+  # Dependencies
+  pillow,
+  pymupdf,
+  wheel,
+  pypdf2,
+}: let
+  version = "1.0.9";
+in
+  buildPythonPackage {
+    inherit version;
+    pname = "pdfcropmargins";
+    pyproject = true;
+
+    src = fetchPypi {
+      inherit version;
+      pname = "pdfCropMargins";
+      hash = "sha256-6bEwTgeVXusKO0QFNPXujA2En/mN1DPbDO4TRr5KBeI=";
+    };
+
+    build-system = [
+      setuptools
+    ];
+
+    dependencies = [
+      pillow
+      pymupdf
+      wheel
+      pypdf2
+    ];
+  }
